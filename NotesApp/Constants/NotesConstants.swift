c3d9383d5990986fcff5ab3160b0ab6cf315fe4f
mehmetalickr <mehmetalickr@outlook.com>
2022-09-20 10:30:21 +0300

    Separate constants

---
+++ NotesApp/Constants/NotesConstants.swift
@@ -0,0 +1,29 @@
+//
+//  NotesConstants.swift
+//  NotesApp
+//
+//  Created by Mehmet Ali ÇAKIR on 13.09.2022.
+//
+
+import UIKit
+
+// MARK: - Notes Constants
+enum NotesStyle {
+    // MARK: - Edit / Done Button Constants
+    static let editButtonTitle = "Edit"
+    static let doneButtonTitle = "Done"
+    // MARK: - Table View Constants
+    static let tableViewCellReuseIdentifier = "noteCell"
+    static let tableViewDeleteActionTitle = "Delete"
+    static let tableViewBackgroundColor = UIColor.black
+    static let tableViewRowHeight: CGFloat = 80
+    static let tableViewCornerRadius: CGFloat = 10.0
+    static let tableViewLeadingTrailingInset: CGFloat = 20
+    static let tableViewTopInset: CGFloat = 200
+    static let tableViewBottomInset: CGFloat = 50
+    // MARK: - Add Note Button Constants
+    static let addNoteButtonImage = "note.text.badge.plus"
+    static let addNoteButtonSize: CGFloat = 20
+    static let addNoteButtonTrailingInset: CGFloat = 30
+    static let addNoteButtonTopInset: CGFloat = 10
+}
---
+++ NotesApp/Constants/NotesDetailConstants.swift
@@ -0,0 +1,21 @@
+//
+//  NotesDetailConstants.swift
+//  NotesApp
+//
+//  Created by Mehmet Ali ÇAKIR on 13.09.2022.
+//
+
+import UIKit
+
+// MARK: - Notes Detail Constants
+enum NotesDetailStyle {
+    // MARK: - Title Text Field Constants
+    static let titleTextFieldFontSize: CGFloat = 25
+    static let titleTextFieldHeight: CGFloat = 50
+    static let titlePlaceholder = "Başlıksız"
+    static var textFieldMaxCharacters = 50
+    // MARK: - Content Text View Constants
+    static let contentTextViewFontSize: CGFloat = 18
+    // MARK: - Button Constants
+    static let buttonTitle = "Save"
+}
